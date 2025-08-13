b3a417fab6a3b8ae7e9e901af2c4d0f6dca41927
AwuriProject <ngoziawurihappy@gmail.com>
2025-08-13 13:59:29 +0100

    backend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
                 throw new Error(`Signup Failed with status ${res.status}`)
             }
             const data = await res.json()
-            console.log(data.errors)
+            console.log(data)
             if(!res.ok){
                 if(data.errors){
                     const errorObj = {}
